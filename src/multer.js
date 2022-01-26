8afdb99bc72995150343be3d76cedb0ae994e0a0
viktorvoltz <vctrchinyeaka@gmail.com>
2022-01-26 12:12:35 +0100

    bug fix

--- src/multer.js
+++ src/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, '../node-post/uploads/')
+        cb(null, '../node-post/assets/')
     },
     filename: function (req, file, cb){
         cb(null, Date.now() + '-' + file.originalname)
